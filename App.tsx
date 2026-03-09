03711d1ad9b1c553b1419b11754018cf2cf85fe5
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-09 17:31:13 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React, { useState, useEffect, useCallback } from 'react';   bowrbvoerjbvor
+import React, { useState, useEffect, useCallback } from 'react';   bowrbvoerjbv
 import { AnalysisResult, User, TrendingFact } from './types';
 import { DetectionEngine } from './components/DetectionEngine';
 import { AnalysisResultCard } from './components/AnalysisResult';
