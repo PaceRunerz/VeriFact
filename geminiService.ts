af783d06916e4ddf6177a471dcf4948aea60c946
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-09 17:32:25 +0530

    Update geminiService.ts

--- geminiService.ts
+++ geminiService.ts
@@ -1,5 +1,5 @@
 
-import { GoogleGenAI, Type } from "@google/genai";
+import { GoogleGenAI, Type } from "@google/genai";      ohbvohub
 import { DetectionType, AnalysisResult, TrendingFact } from "./types";
 import { MOCK_TRENDING } from "./constants";
 
