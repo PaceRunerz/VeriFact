a20b45b490831bad51da6fd55ac7265029706404
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-10 14:17:28 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,7 +1,7 @@
 
 import { TrendingFact } from "./types";
 
-export const COLORS = {        jnijnivd
+export const COLORS = {        jnijni
   primary: '#0A192F',
   secondary: '#64FFDA',
   verified: '#10B981',
