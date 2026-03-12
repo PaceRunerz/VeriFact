49abe0e61663f50813a6be18bfced71e03ca58b9
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-12 23:10:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 
-import { defineConfig } from 'vite';      jhvlkhvluh
+import { defineConfig } from 'vite';      jhvlkhvl
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
