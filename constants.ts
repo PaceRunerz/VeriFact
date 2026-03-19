275efe15a232c466a9a2b71e1962494c798bdfcf
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-19 22:10:36 +0530

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -2,7 +2,7 @@
 import { TrendingFact } from "./types";
 
 export const COLORS = {        
-  primary: '#0A192F',            srbdbv
+  primary: '#0A192F',            srbdb
   secondary: '#64FFDA',
   verified: '#10B981',     
   misinformation: '#EF4444',
