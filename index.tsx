a19af94198783199d3466337d09680298d916d00
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-20 13:42:30 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
      
-const rootElement = document.getElementById('root');      knxjb 
+const rootElement = document.getElementById('root');      knx
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
