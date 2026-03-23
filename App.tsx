bc05e43d38abea95de023a00e2bd6176ce9603e5
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-23 18:36:15 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React, { useState, useEffect, useCallback } from 'react';   
-import { AnalysisResult, User, TrendingFact } from './types';    iru
+import { AnalysisResult, User, TrendingFact } from './types';    ir
 import { DetectionEngine } from './components/DetectionEngine';
 import { AnalysisResultCard } from './components/AnalysisResult';
 import { TrendingSidebar } from './components/TrendingSidebar';
