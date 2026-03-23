6f6970d7fd484297bbd3f85cc038e69691732316
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-23 18:40:31 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -3,7 +3,7 @@ export interface User {
   id: string;             
   name: string;
   email: string;           
-  photoURL: string;        jhsb
+  photoURL: string;        jhs
   iqScore: number;
 }
 
