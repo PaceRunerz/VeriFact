11e8d97ca1d207b42611cf49e07569c4609edaed
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-23 18:39:11 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 
 import { defineConfig } from 'vite';      
-import react from '@vitejs/plugin-react'; fs
+import react from '@vitejs/plugin-react'; f
 
 export default defineConfig({
   plugins: [react()],
