ff433aeb5378bb63d4a2042cfd8323c925ae7638
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-23 18:37:57 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,8 +2,7 @@
 import React from 'react';  
 
 import ReactDOM from 'react-dom/client';
-import App from './App';   jhb
-     
+import App from './App';   jh
 const rootElement = document.getElementById('root');      
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
