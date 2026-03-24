6090b2f4bb2a22f397384f349d28094fcaea2d35
S.Tripathi <166680926+PaceRunerz@users.noreply.github.com>
2026-03-24 14:07:51 +0530

    Update geminiService.ts

--- geminiService.ts
+++ geminiService.ts
@@ -1,7 +1,7 @@
 
 import { GoogleGenAI, Type } from "@google/genai";      
 import { DetectionType, AnalysisResult, TrendingFact } from "./types";
-import { MOCK_TRENDING } from "./constants";    iug
+import { MOCK_TRENDING } from "./constants";    iu
 
 
 const PRIMARY_MODEL = "gemini-3-flash-preview";        
